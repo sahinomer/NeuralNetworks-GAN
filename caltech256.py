8d00b9638e728a384d1d69d873efa9d9aebe8c22
sahinomer <sahin.omer@outlook.com>
2019-12-11 22:33:23 +0300

    Remove empty rows, shuffle dataset

--- caltech256.py
+++ caltech256.py
@@ -1,6 +1,7 @@
 import os
 
 import numpy as np
+from sklearn.utils import shuffle
 
 from keras.utils import get_file
 from keras_preprocessing.image import load_img, img_to_array
@@ -27,7 +28,7 @@ def load_data(width=128, height=128, verbose=False):
         category_path = path + '\\' + category + '\\'
         image_list = os.listdir(category_path)
 
-        for image in image_list:
+        for image in image_list[:100]:
             if os.path.isfile(category_path + image):
                 img = load_img(category_path + image)
                 img = img_to_array(img)
@@ -37,8 +38,10 @@ def load_data(width=128, height=128, verbose=False):
                 image_label[sample] = int(category.split('.')[0])
                 sample += 1
 
-    np.random.shuffle(image_data)
+    image_data = image_data[:sample, :, :, :]
+    image_label = image_label[:sample]
 
+    image_data, image_label = shuffle(image_data, image_label, random_state=0)
     return image_data, image_label
 
 
